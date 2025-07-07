b1a0501e25c464aa01c550761b1d86615c8fb200
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2025-07-07 20:25:47 +0530

    first commit

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export function signup(req, res) {
+    res.send("Signup endpoint");
+}
+
+export function login(req,res){
+    res.send("Login endpoint");
+}
+
+export function logout(req, res) {
+    res.send("Logout endpoint");
+}
