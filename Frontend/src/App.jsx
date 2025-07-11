b1b3ee685d7fbf54ecac71f27483238938676979
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2025-07-11 23:13:11 +0530

    added routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Route, Routes } from "react-router"
+import HomePage from './pages/HomePage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import NotificationPage from './pages/NotificationPage.jsx';
+import CallPage from './pages/CallPage.jsx';
+import ChatPage from './pages/ChatPage.jsx';
+import OnBoardingPage from './pages/OnBoardingPage.jsx';
 
 function App() {
   return (
     <div className="" data-theme = "night">
-      
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/signup" element={<SignUpPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/notification" element={<NotificationPage />} />
+        <Route path="/call" element={<CallPage/>} />
+        <Route path="/chat" element={<ChatPage/>} />
+        <Route path="/onboarding" element={<OnBoardingPage/>} />
+      </Routes>
     </div>
   );
 }
