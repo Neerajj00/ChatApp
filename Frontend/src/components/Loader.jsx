8e28b3353e7a7ab17690bec25851510c813b37e1
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2025-07-15 15:05:38 +0530

    feat: enhance Loader component to support dynamic theme

--- Frontend/src/components/Loader.jsx
+++ Frontend/src/components/Loader.jsx
@@ -1,9 +1,12 @@
 import { LoaderIcon } from 'lucide-react'
 import React from 'react'
+import { useTheme } from './../store/useTheme';
 
 function Loader() {
+  const { theme } = useTheme();
+
   return (
-    <div className='min-h-screen flex items-center justify-center'>
+    <div className='min-h-screen flex items-center justify-center' data-theme={theme}>
             <LoaderIcon className='animate-spin size-10 text-primary'>
 
             </LoaderIcon>
