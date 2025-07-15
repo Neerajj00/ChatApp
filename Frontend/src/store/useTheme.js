4adfb63754e6f948c66289c3cf689487e3e88e8a
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2025-07-15 15:02:29 +0530

    feat: persist theme selection in local storage

--- Frontend/src/store/useTheme.js
+++ Frontend/src/store/useTheme.js
@@ -1,6 +1,9 @@
 import { create } from 'zustand'
 
 export const useTheme = create((set) => ({
-    theme:"night",
-    setTheme: (theme) => set({theme})
+    theme: localStorage.getItem("theme") || "night",
+    setTheme: (theme) => {
+        set({theme})
+        localStorage.setItem("theme", theme);
+    }
 }))
