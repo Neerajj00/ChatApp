bbaa4e0996ab602998464e8d8ceb2e6c67ae69ed
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2025-07-17 22:20:11 +0530

    capitalized first letter of frontend

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -27,11 +27,11 @@ app.use('/api/chat', chatRoutes);
 
 if(process.env.NODE_ENV === 'production') {
     // Serve static files from the React app
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.use(express.static(path.join(__dirname, '../Frontend/dist')));
 
     // Handle React routing, return all requests to React app
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+        res.sendFile(path.join(__dirname, '../Frontend/dist', 'index.html'));
     });
 }
 
