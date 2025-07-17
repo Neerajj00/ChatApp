8fe931f904f49cf2eb0f6faf43c56132c82e40fc
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2025-07-17 22:04:42 +0530

    last and final commit (changed package.json to make the app ready for deployment)

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -6,11 +6,13 @@ import chatRoutes from "./routes/chat.route.js";
 import connectDb from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -22,6 +24,16 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes );
 app.use('/api/users', userRoutes );
 app.use('/api/chat', chatRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+    // Serve static files from the React app
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    // Handle React routing, return all requests to React app
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT} `);
